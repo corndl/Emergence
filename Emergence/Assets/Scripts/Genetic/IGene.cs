6332ff3d033382fd760a9be05022769b76768e65
SlyWax <slyxwax@gmail.com>
2015-08-22 19:57:07 +0200

    Génétique

---
+++ Emergence/Assets/Scripts/Genetic/IGene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IGene {
+
+	// Chargement des genes
+	void set(string sgene);
+	// save gene
+	string get ();
+	// children
+	IGene children();
+
+	// type gene (int, list)
+	// range
+	// % chance de mutation
+
+
+}
---
+++ Emergence/Assets/Scripts/Genetic/IntGene.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class IntGene : IGene {
+	// type gene (int, list)
+	string type = "int";
+	// valeur
+	public int value;
+	// range
+	int min;
+	int max;
+	// % chance de mutation
+	float mutationChance;
+
+
+	// Chargement des genes
+	public void set(string sgene){
+		
+	}
+	// serialize gene
+	public string get (){
+		return type ;
+//		return type +" "+ min.ToString +" "+ max.ToString +" "+ mutationChance.ToString ;
+	}
+	// children
+	public IGene children(){
+		
+		return (IGene)this.intChildren();
+	}
+	/// <summary>
+	/// Create a child with possible mutation the children.
+	/// </summary>
+	/// <returns>The children.</returns>
+	public IntGene intChildren(){
+		IntGene child = (IntGene)this.MemberwiseClone();
+		// si on a la chance de muter
+		if ( Random.Range(0.0F, 1.0F) < mutationChance ){
+			// on mute au hasard dans l'interval
+			child.value = Random.Range(min, max+1);
+		}
+		return child;
+	}
+
+}
